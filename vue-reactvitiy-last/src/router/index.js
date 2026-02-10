d2a7d4a70a346d4e2ab8ab6809975c7c17475ac1
veeveejeebies <vavinz@nycstudents.net>
2026-02-10 12:53:09 -0500

    d

--- vue-reactvitiy-last/src/router/index.js
+++ vue-reactvitiy-last/src/router/index.js
@@ -1,8 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
 
+import Project from '@/views/Project.vue' 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+ {
+      path: '/',
+      name: 'home',
+      component: Project,
+    },
+
+  ],
 })
 
 export default router
