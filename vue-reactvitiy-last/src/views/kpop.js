f653f5765ba03065aa85c8ec3115e63e80b2899a
veeveejeebies <vavinz@nycstudents.net>
2026-02-11 12:31:40 -0500

    d

---
+++ vue-reactvitiy-last/src/views/kpop.js
@@ -0,0 +1,106 @@
+export const store = reactive({
+  products: [
+    {
+      name: "Strawberry Cupcake",
+      url: "https://i.pinimg.com/originals/bf/f2/34/bff23415adae94de5a3c5cef14915f96.jpg",
+      price: 3,
+    },
+    {
+      name: "Lemon Cupcake",
+      url: "https://i.pinimg.com/originals/57/e1/b5/57e1b5365966fac68f93eb896f8e1c38.jpg",
+      price: 3,
+    },
+    {
+      name: "Jelly Bean Cupcake",
+      url: "https://i.pinimg.com/originals/f6/70/b6/f670b689dc38796f1d5ca344c05ea556.jpg",
+      price: 3,
+    },
+    {
+      name: "Ice Cream Sundae",
+      url: "https://i.pinimg.com/originals/6f/c9/cd/6fc9cd7954333b334bc012a9073caedd.jpg",
+      price: 3,
+    },
+    {
+      name: "Strawberry Soda",
+      url: "https://i.pinimg.com/originals/07/fe/ce/07fece59f75c90aded002bf17d7c678f.jpg",
+      price: 2,
+    },
+    {
+      name: "Watermelon Soda",
+      url: "https://i.pinimg.com/originals/4d/9c/3d/4d9c3d462d51a5fdc7e0e0dc1291171f.jpg",
+      price: 2,
+    },
+    {
+      name: "Peach Soda",
+      url: "https://i.pinimg.com/originals/a4/fe/fa/a4fefad41bd78b2b6726fed804888da9.jpg",
+      price: 2,
+    },
+    {
+      name: "Black Tea",
+      url: "https://i.pinimg.com/originals/0a/58/73/0a58737d88eaf4590b12d115040d7a6a.jpg",
+      price: 2,
+    },
+    {
+      name: "Cherry Pudding",
+      url: "https://i.pinimg.com/originals/13/4c/97/134c97fa660d44cf05ed287af7f43e32.jpg",
+      price: 4,
+    },
+    {
+      name: "Strawberry Pudding",
+      url: "https://i.pinimg.com/originals/ba/d4/c8/bad4c8883f0bc78eb3a6c19158942632.jpg",
+      price: 4,
+    },
+    {
+      name: 
+      url: 
+      price: 
+    },
+    {
+      name: 
+      url: 
+      price: 
+    },
+    {
+      name: 
+      url: 
+      price: 
+    },
+    {
+      name: 
+      url:
+      price: 
+    },
+    {
+      name:
+      url: 
+      price: 
+    },
+    {
+      name:
+      url:
+      price: 
+    },
+    {
+      name:
+      url:
+      price: 
+    },
+    {
+      name: 
+      url: 
+      price: 
+    },
+    {
+      name: 
+      url: 
+      price: 
+    },
+    {
+      name: 
+      url: 
+      price: 
+    },
+  ],
+  cart: [],
+  total: 0,
+});
