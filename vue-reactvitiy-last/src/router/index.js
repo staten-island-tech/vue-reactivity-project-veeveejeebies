fc3c0bd872d0bf960ed90a7744f558fad630a1b4
veeveejeebies <vavinz@nycstudents.net>
2026-02-12 12:34:01 -0500

    d

--- vue-reactvitiy-last/src/router/index.js
+++ vue-reactvitiy-last/src/router/index.js
@@ -1,12 +1,12 @@
 
-import Project from '@/views/Project.vue' 
+import albums from '@/views/albums.vue' 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
  {
       path: '/',
       name: 'home',
-      component: Project,
+      component: albums,
     },
 
   ],
