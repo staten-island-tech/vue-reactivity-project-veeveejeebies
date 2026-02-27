c5b3dc8049a0fff0a3b08bdc1f1bb1523c1c523c
veeveejeebies <vavinz@nycstudents.net>
2026-02-27 12:22:37 -0500

    d

--- vue-reactvitiy-last/src/router/index.js
+++ vue-reactvitiy-last/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import albums from '@/App.vue' 
+import albums from '@/Albums.vue' 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
